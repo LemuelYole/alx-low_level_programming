933c6f3f386821371f08ba9ed0fb78abddaa210f
Lemuelyole <lemuelyole@gmail.com>
2022-09-10 11:24:19 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+
+/**
+ * main - pointing
+ * Return always 0 (sunccessful)
+ */
+int (main)void
+{
+	puts("\"Programming is like building a multilingua puzzle");
+	return (0);
+}
