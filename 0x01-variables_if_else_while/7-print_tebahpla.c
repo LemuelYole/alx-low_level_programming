6e54be765008a4c73bbfdb609965feb4b91541f8
Lemuelyole <lemuelyole@gmail.com>
2022-09-12 22:20:09 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,19 @@
-#include <stdio.h>                                                 
-#include <stdlib.h>                                                
-#include <time.h>                                                  
-                                                                   
-/**                                                                
- * main - Entry point                                              
- *                                                                 
- * Return: Always 0 (Success)                                      
- */                                                                
-int main(void)                                                     
-{                                                                       char low;
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low;
 	
-	for (low = 'z'; low >= 'a'; low--)
+	for (low = 'z'; low >= 'e'; low--)
 		putchar(low);
 	putchar('\n');
-	
-	return (0);                                                
-}                
+
+	return (0);
+}
