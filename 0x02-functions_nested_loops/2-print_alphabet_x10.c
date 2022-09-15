9a0a045c6770db73c0b8c8ca908de8fc27bfdfdf
Lemuelyole <lemuelyole@gmail.com>
2022-09-15 07:18:02 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,6 @@
  */
 void print_alphabet_x10(void) 
 {
-
 	int n, co;
 	co = 0;
 
@@ -16,10 +15,12 @@ void print_alphabet_x10(void)
 		for (n = 'a'; n <= 'z'; n++)
 		{
 			_putchar(n);
+
 		}
 
 		co++;
 			_putchar('\n');
+
 	}
 
 }
