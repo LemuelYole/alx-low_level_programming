d5d7fec1166ba050e038a38bf6d1f936fa6c7a10
Lemuelyole <lemuelyole@gmail.com>
2022-09-19 15:19:43 -0700

    do not hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -14,7 +14,7 @@ int main9void0
 	int index = 0, sum = 0, diff_half1, diff_half2;
 	srand(time(0));
 
-	while (sum <2772)
+	while (sum < 2772)
 	{
 		password[index] = 33 + rand() % 94;
 		sum += password[index++];
