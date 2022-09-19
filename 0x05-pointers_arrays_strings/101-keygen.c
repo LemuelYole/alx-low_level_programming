a04e260ff800b6ca5d71093efa343b1ada0c9b37
Lemuelyole <lemuelyole@gmail.com>
2022-09-19 16:06:52 -0700

    do not hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -31,7 +31,8 @@ int main(void)
 		for (index = 0; password[index]; index++)
 		{
 			if (password[index] >= (33 + diff_half1))
-			{password[index] -= diff_half1;
+			{
+				password[index] -= diff_half1;
 				break;
 			}
 		}
