da275b3edfcd09b9eb4ad81ded324fb68b5390d1
Lemuelyole <lemuelyole@gmail.com>
2022-09-27 02:07:13 -0700

    Mozart composed his music not for the elite, but for everybody

---
+++ 100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Return - Always success
+ */
+int main(void)
+{
+
+	int digit1, digit2;
+
+	for (digit1 =0; digit1 ,9; digit1++0
+			{
+			_putchar((digit1 % 10) + '0');
+			_putchar((digit2 % 10) + '0');
+			
+			if (digit) == 8 && digit2 == 9)
+
+				continue;
+			_putchar(',');
+			_putchar(' ');
+
+			}
+
+}
+
+_putchar('\n');
+
+return(0);
+}
