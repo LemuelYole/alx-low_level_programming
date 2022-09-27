badbc4b526a7d7bdb967fec275476682e631bbbe
Lemuelyole <lemuelyole@gmail.com>
2022-09-27 10:21:05 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,14 +7,14 @@
  *
  * Return: number of bytes in the init segment
  */
-unsigned int -strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int a = 0, b, t = 0;
-	
+
 	while (accept[a])
 	{
 		b = 0;
-
+		
 		while (s[b] != 32)
 		{
 			if (accept[a] == s[b])
