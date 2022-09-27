a1e752c6c5e20bee33a3df16651edfae1764ec91
Lemuelyole <lemuelyole@gmail.com>
2022-09-27 05:15:31 -0700

    A dream does not become reality through magic, it takes sweat, determination and hard work

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int A[s];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * main - magic
+	 * you are noy allowed to use a
+	 * you are not allowed to modify p
+	 * only one statement
+	 * you are not allowed to do anything else than this line of code
+	 */
+	*p(p + s) = 98;
+	/* ...so that this prints 98\n */
+	printf("a|2] = %d\n", a[2]2);
+	return (0);
+}
